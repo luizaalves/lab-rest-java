b2cde7c6a3474adb2d5cb3facaaee76708e1e6cb
luiza <l.uhzinha@hotmail.com>
2019-10-16 11:21:40 -0300

    web

---
+++ src/main/java/engtelecom/std/rest/PrimeiroExemplo.java
@@ -0,0 +1,54 @@
+package engtelecom.std.rest;
+import engtelecom.std.entities.Pessoa;
+import javax.ws.rs.*;
+import javax.ws.rs.core.*;
+@Path("exemplo")
+public class PrimeiroExemplo {
+    @GET
+    @Produces(MediaType.TEXT_PLAIN + ";charset=utf-8")
+    public Response olaMundo() {
+        String mensagem = "Ol´a mundo";
+        return Response.ok(mensagem).build();
+    }
+    @Path("{nome}")
+    @GET
+    @Produces(MediaType.TEXT_PLAIN + ";charset=utf-8")
+    public Response olaNome(@PathParam("nome") String nome) {
+        String mensagem = "Ol´a " + nome;
+        return Response.ok(mensagem).build();
+    }
+    @Path("pessoas")
+    @GET
+    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    public Response obtemPessoa() {
+        Pessoa p = new Pessoa("Fulano", "email@email.com");
+        return Response.ok(p).build();
+    }
+    @Path("pessoas")
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response addPessoa(Pessoa p, @Context UriInfo uriInfo) {
+// Retornando o c´odigo HTTP 201 com a URL do recurso criado no campo Location
+// do cabe¸calho HTTP
+        UriBuilder builder = uriInfo.getAbsolutePathBuilder();
+        builder.path(p.getNome());
+        return Response.created(builder.build()).build();
+    }
+    @Path("pessoas")
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response updatePessoa(Pessoa p) {
+        return Response.ok("Pessoa atualizada").build();
+    }
+    @Path("pessoas/{pessoa}")
+    @DELETE
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response removePessoa(@PathParam("pessoa") String nome) {
+        if (nome.equalsIgnoreCase("Fulano")) {
+            return Response.ok("Pessoa exclu´ıda").build();
+        }
+        throw new NotFoundException();
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,22 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>Laboratorio com servicos web RESTful em Java</title>
+  </head>
+  <body>
+<h1>Laboratório com serviços web RESTful em Java</h1>
+
+<H2>Primeiro exemplo</H2>
+
+<ul>
+  <li><a href="/api/v1/exemplo">Olá mundo</a></li>
+  <li><a href="/api/v1/exemplo/Fulano">Olá Seu nome na URL</a></li>
+  <li><a href="/api/v1/exemplo/pessoas">Dados de uma pessoa</a></li>
+</ul>
+
+<h2>Gestao de laboratórios</h2>
+<ul>
+  <li><a href="/api/v1/campi">Lista com todos os campi</a></li>
+</ul>
+</body>
+</html>
